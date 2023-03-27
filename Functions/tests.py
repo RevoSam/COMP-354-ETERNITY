a9dd356deb1fdbbf8e3249ce9d138910c76cee4b
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-03-27 19:06:03 -0400

    add tests for gamma

--- Functions/tests.py
+++ Functions/tests.py
@@ -1,2 +1,18 @@
 # unit tests for all functions
 
+import unittest
+from gamma import *
+
+class Test_Gamma(unittest.TestCase):
+
+    def test_gamma_valid_input(self):
+        self.assertEqual(round(gamma(0.5), 10), 1.7724538509)
+        self.assertEqual(round(gamma(1), 10), 1)
+        self.assertEqual(round(gamma(2), 10), 1)
+        self.assertEqual(round(gamma(5), 10), 24)
+    
+    def test_gamma_invalid_input(self):
+        self.assertEqual(gamma(0), None)
+
+if __name__ == '__main__':
+    unittest.main()
