447f398cbffd40140eee7caf0a48382f46482b07
Chuanqi-123 <114367337+Chuanqi-123@users.noreply.github.com>
2023-03-29 16:29:45 -0400

    removed built-in functions

---
+++ Functions/hyperbolic_fn.py
@@ -0,0 +1,11 @@
+from subordinate_fn import *
+
+# hyperbolic sine function
+def sinh(x):
+    return (exp(x) - exp(-x)) / 2
+
+# ------testing-------
+
+# print(sinh(0))
+# print(sinh(1))
+# print(sinh(PI))
